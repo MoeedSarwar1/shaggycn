6cd3b91b7b902f5d1a9d2a04e078c3c71bc0ea3a
Moeed Sarwar <oyemoeed@gmail.com>
2025-06-03 04:48:18 +0500

    remove unused colors

--- src/theme/context/themeContext.tsx
+++ src/theme/context/themeContext.tsx
@@ -1,19 +1,14 @@
 import React, {createContext, ReactNode, useMemo, useState} from 'react';
-import colors, {darkColors} from '../colors';
 import {getDarkPalette, getLightPalette} from '../palette';
 
 const lightPalette = getLightPalette();
 const darkPalette = getDarkPalette();
 
 export const lightTheme = {
-  dark: false,
-  colors, // from colors.ts (light colors)
   palette: lightPalette,
 };
 
 export const darkTheme = {
-  dark: true,
-  colors: darkColors, // from colors.ts (dark colors)
   palette: darkPalette,
 };
 
