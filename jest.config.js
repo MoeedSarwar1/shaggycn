de0e75bbcad16c69810bcf2cf53383eafe46a238
Moeed Sarwar <moeedsarwar112@gmail.com>
2025-06-13 00:39:51 +0500

    test

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,3 @@
 module.exports = {
   preset: 'react-native',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  testMatch: ['**/__tests__/**/*.(test|spec).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}', // Include all TS/TSX files in src/
-    '!src/**/*.d.ts', // Exclude type declarations
-    '!src/**/__tests__/**', // Exclude test files
-    '!src/**/index.ts', // Exclude barrel/index files
-    '!src/**/types/**', // Exclude types folders
-    '!src/**/constants/**', // Exclude constants if desired
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
 };
